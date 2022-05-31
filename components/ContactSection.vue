4700bcc7fb9a31144651df6837d272b1c8cae68d
Kevin <kyang315@yahoo.com>
2022-05-31 16:42:55 -0400

    contact section update

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -3,30 +3,44 @@
     <v-container class="text-left">
       <div class="display-3 text-center">Contact</div>
       <v-divider class="my-8 mx-16 pb-8" />
-      <p class="teal-indent">
-        Interested in contacting me? Please either send me a message on LinkedIn
-        or shoot me an email.
+      <p class="text-center">
+        Interested in contacting me?<br />
+        Feel free to reach out to me on LinkedIn or send me an email at the
+        links below:
       </p>
-      <div>
-        <v-btn
-          fab
-          small
-          class="mx-1"
-          :href="$store.state.me.linkedin_url"
-          target="_blank"
-        >
-          <v-icon>mdi-linkedin</v-icon>
-        </v-btn>
-        <v-btn fab small class="mx-1" :href="`mailto:` + $store.state.me.email">
-          <v-icon>mdi-email</v-icon>
-        </v-btn>
-      </div>
+      <table class="ma-auto pt-4">
+        <tr height="50">
+          <td>
+            <v-btn
+              fab
+              small
+              class="mx-1 _darkteal mr-4"
+              :href="$store.state.me.linkedin_url"
+              target="_blank"
+            >
+              <v-icon>mdi-linkedin</v-icon>
+            </v-btn>
+          </td>
+          <td>
+            {{ $store.state.me.linkedin_url }}
+          </td>
+        </tr>
+        <tr height="50">
+          <td>
+            <v-btn
+              fab
+              small
+              class="mx-1 _darkteal mr-4"
+              :href="`mailto:` + $store.state.me.email"
+            >
+              <v-icon>mdi-email</v-icon>
+            </v-btn>
+          </td>
+          <td>
+            {{ $store.state.me.email }}
+          </td>
+        </tr>
+      </table>
     </v-container>
   </v-container>
 </template>
-<style scoped>
-.teal-indent {
-  border-left: 4px solid #66fcf1;
-  padding-left: 12px;
-}
-</style>
