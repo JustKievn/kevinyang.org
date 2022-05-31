1104eb8cec634c74fc1a937809f34dfae2ee5bbc
Kevin <kyang315@yahoo.com>
2022-05-30 22:25:32 -0400

    header config
    title update
    added description

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,15 +9,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: 'Kevin Yang',
-    title: 'vercel-nuxt',
+    title: 'Kevin Yang',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: "Kevin's a great guy and you should hire him C:" },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
