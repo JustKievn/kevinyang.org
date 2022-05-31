b32d0ddf78a36e18f52d8ebeed7e15ff797dc970
Kevin <kyang315@yahoo.com>
2022-05-31 00:40:23 -0400

    About Me

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -1,5 +1,37 @@
 <template>
-  <v-container id="section-about" class="text-center" fluid>
-    About Me
+  <v-container id="section-about" class="py-16" fluid>
+    <v-container class="text-left">
+      <div class="display-3 text-center">About Me</div>
+      <v-divider class="my-8 mx-16 pb-8" />
+      <p class="title">Hello! My name is Kevin Yang.</p>
+      <br />
+      <p class="teal-indent">
+        I am a first-generation Asian-American born and raised in Philadelphia,
+        PA. I received my Bachelors in Computer Engineering from Drexel
+        University in 2019 as part of their 5-Year Co-op Program. During my stay
+        at Drexel, I also worked as a developer for Comcast for the last three
+        years of my schooling. Balancing school and work was no easy task, but I
+        believe the skills and experience I gained from those three years was
+        invaluable. My ability to multitask, prioritize, and manage stress was
+        put to the test and I became a better engineer as a result.
+      </p>
+      <br />
+      <p class="teal-indent">
+        I'm currently based in Mount Laurel, NJ and work as a Software Engineer
+        for Comcast. I love the work I do and cannot imagine me pursuing any
+        other career path. I enjoy tackling complex problems and breaking them
+        down into smaller, consumable pieces and I don't think any other
+        industry puts those skills to the test more than Software Engineering. I
+        love that software engineering is a rapidly evolving field and that I
+        will be experiencing it first-hand as I learn the nuisances of
+        engineering as it evolves.
+      </p>
+    </v-container>
   </v-container>
 </template>
+<style scoped>
+.teal-indent {
+  border-left: 4px solid #66fcf1;
+  padding-left: 12px;
+}
+</style>
