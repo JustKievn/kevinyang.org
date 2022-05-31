2592aa1659f5495bea46eddb31f8604da4f84eb7
Kevin <kyang315@yahoo.com>
2022-05-31 16:42:34 -0400

    footer update

--- components/NavFooter.vue
+++ components/NavFooter.vue
@@ -1,7 +1,8 @@
 <template>
   <v-footer id="section-footer">
     <v-col class="text-center py-2">
-      Made with <v-icon color="red"> mdi-heart </v-icon>
+      Made with <v-icon color="red"> mdi-heart </v-icon> by
+      <span class="_teal--text"> Kevin Y. </span>
     </v-col>
   </v-footer>
 </template>
