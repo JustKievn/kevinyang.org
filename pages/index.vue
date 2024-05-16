1c8d7dcdb787f3af3a483d0639379ee2e72f0cb7
Kevin <keyang315@gmail.com>
2024-05-16 01:05:28 -0400

    remove page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,8 @@
 <template>
   <v-container fluid class="ma-0 pa-0">
-    <LandingSection />
-    <AboutSection class="_gray" />
-    <SkillsSection class="_black" />
-    <ExperienceSection class="_gray" />
-    <ContactSection class="_black" />
+    <div>
+      Hello!
+    </div>
     <NavFooter />
   </v-container>
 </template>
